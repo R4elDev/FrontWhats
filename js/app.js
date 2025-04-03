5602c2acd3480b041f68a6ce9bbabdd693b94c3c
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-04-03 11:39:48 -0300

    Criado a lista de contatos puxando pelo Array

--- js/app.js
+++ js/app.js
@@ -5,8 +5,53 @@ async function carregarContatosApi(){
     const response = await fetch(url)
 
     const data = await response.json()
-    console.log(response)
+    
+    return data
+}
+
+async function criarLiDeContatos() {
+    const dadosContatos = await carregarContatosApi()
+
+    const ul = document.getElementById('ulContatos')
+
+    dadosContatos.forEach(function(item){
+        const liContato = document.createElement('li')
+        liContato.classList.add('contato')
+        const divImage = document.createElement('div')
+        divImage.classList.add('imagemContato')
+        
+        const divTextosCont = document.createElement('div')
+        divTextosCont.classList.add =('textosContainers')
+        const h1ListaContato = document.createElement('h1')
+        const pListaContato = document.createElement('p')
+
+        h1ListaContato.textContent = item.nome
+        pListaContato.textContent = item.descricao
+
+
+
+        liContato.appendChild(divImage)
+        divTextosCont.appendChild(h1ListaContato)
+        divTextosCont.appendChild(pListaContato)
+        liContato.appendChild(divTextosCont)
+        ul.appendChild(liContato)
+
+    })
+
+}
+
+async function carregarDadosConversasApi(nome) {
+    const url = `https://projeto-whats-marcel.onrender.com/v1/whatsapp/conversas-cada-usuario/cont/11966578996?cont=${nome}`
+    const response = await fetch(url)
+    
+    const data = await response.json()
+
+    return data
 }
 
 
-carregarContatosApi()
+
+
+
+criarLiDeContatos()
+carregarDadosConversasApi('josé maria da silva')
