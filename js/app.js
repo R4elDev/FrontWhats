b517b1dea7d67fa7f5e589f230c03c11ad1e8f9f
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-04-03 11:16:08 -0300

    Terminando de marcar e estilizar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+async function carregarContatosApi(){
+    const url = `https://projeto-whats-marcel.onrender.com/v1/whatsapp/contatos-para-usuarios/11966578996`
+    const response = await fetch(url)
+
+    const data = await response.json()
+    console.log(response)
+}
+
+
+carregarContatosApi()
